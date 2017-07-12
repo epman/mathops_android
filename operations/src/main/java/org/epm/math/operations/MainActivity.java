9f7737912c2c30e15b100cb8856c1f4c9bb6ee54
Emilio Mancini <epmancini@gmail.com>
2017-07-12 17:46:42 +0200

    End game smiles from www.openclipart.org

--- operations/src/main/java/org/epm/math/operations/MainActivity.java
+++ operations/src/main/java/org/epm/math/operations/MainActivity.java
@@ -10,8 +10,10 @@ import android.support.annotation.Nullable;
 import android.support.annotation.UiThread;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.AsyncTaskLoader;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.content.Loader;
 import android.support.v4.widget.TextViewCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Gravity;
 import android.view.View;
@@ -40,30 +42,32 @@ public final class MainActivity extends AppCompatActivity implements LoaderManag
     private final Handler handler = new Handler();
 
     @IdRes
-    private final static int[] textViewsOp = {
+    private final static int[] textSwitchersOp = {
             R.id.textView1_1, R.id.textView1_2, R.id.textView1_sign,
             R.id.textView2_1, R.id.textView2_2, R.id.textView2_sign,
     };
     @IdRes
-    private final static int[] textViewsRes = {
+    private final static int[] textSwitchersRes = {
             R.id.textView3_1, R.id.textView3_2, R.id.textView3_3
     };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Math.hideSystemUI(getWindow().getDecorView());
+        //Math.hideSystemUI(getWindow().getDecorView());
 
         getSupportLoaderManager()
                 .initLoader(0, null, this);
 
         operations = new Operations(this);
 
-        for (final @IdRes int tvid: textViewsOp) {
+        for (final @IdRes int tvid: textSwitchersOp) {
             findViewById(tvid).setOnClickListener(newOpOnClickListener);
+            initTextSwitcher(tvid, textSwitcherFactoryOps);
         }
-        for (final @IdRes int tvid: textViewsRes) {
+        for (final @IdRes int tvid: textSwitchersRes) {
             findViewById(tvid).setOnClickListener(deleteClickListener);
+            initTextSwitcher(tvid, textSwitcherFactoryOps);
         }
         findViewById(R.id.buttonChangeOperation).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -73,7 +77,7 @@ public final class MainActivity extends AppCompatActivity implements LoaderManag
             }
         });
 
-        initTextSwitcher(R.id.textSwitcerPoints);
+        initTextSwitcher(R.id.textSwitcerPoints, textSwitcherFactoryPoints);
 
         btns[0] = (Button) findViewById(R.id.button0);
         btns[1] = (Button) findViewById(R.id.button1);
@@ -119,25 +123,40 @@ public final class MainActivity extends AppCompatActivity implements LoaderManag
     //endregion
 
 
+    private ViewSwitcher.ViewFactory textSwitcherFactoryOps = new ViewSwitcher.ViewFactory() {
 
-    private void initTextSwitcher(@IdRes final int tsId) {
+        public View makeView() {
+            final TextView myText = new TextView(MainActivity.this);
+            FrameLayout.LayoutParams layoutparams1 = new FrameLayout.LayoutParams(
+                    FrameLayout.LayoutParams.WRAP_CONTENT, FrameLayout.LayoutParams.MATCH_PARENT);
+            myText.setLayoutParams(layoutparams1);
+
+            myText.setGravity(Gravity.CENTER_VERTICAL);
+            TextViewCompat.setTextAppearance(myText, R.style.BigFont);
+            myText.setTextColor(ContextCompat.getColor(MainActivity.this, R.color.colorPrimaryDark));
+            return myText;
+        }
+    };
+    private ViewSwitcher.ViewFactory textSwitcherFactoryPoints = new ViewSwitcher.ViewFactory() {
+
+        public View makeView() {
+            final TextView myText = new TextView(MainActivity.this);
+            FrameLayout.LayoutParams layoutparams1 = new FrameLayout.LayoutParams(
+                    FrameLayout.LayoutParams.WRAP_CONTENT, FrameLayout.LayoutParams.MATCH_PARENT);
+            myText.setLayoutParams(layoutparams1);
+
+            myText.setGravity(Gravity.CENTER_VERTICAL);
+            TextViewCompat.setTextAppearance(myText, org.epm.math.R.style.TextAppearance_AppCompat_Large);
+            myText.setTextColor(Color.WHITE);
+            return myText;
+        }
+    };
+
+    private void initTextSwitcher(@IdRes final int tsId, final ViewSwitcher.ViewFactory vf) {
         final TextSwitcher ts = (TextSwitcher)findViewById(tsId);
 
         // Set the ViewFactory of the TextSwitcher that will create TextView object when asked
-        ts.setFactory(new ViewSwitcher.ViewFactory() {
-
-            public View makeView() {
-                final TextView myText = new TextView(MainActivity.this);
-                FrameLayout.LayoutParams layoutparams1 = new FrameLayout.LayoutParams(
-                        FrameLayout.LayoutParams.WRAP_CONTENT, FrameLayout.LayoutParams.MATCH_PARENT);
-                myText.setLayoutParams(layoutparams1);
-
-                myText.setGravity(Gravity.CENTER_VERTICAL);
-                TextViewCompat.setTextAppearance(myText, org.epm.math.R.style.TextAppearance_AppCompat_Large);
-                myText.setTextColor(Color.WHITE);
-                return myText;
-            }
-        });
+        ts.setFactory(vf);
 
         // Declare the in and out animations and initialize them
         final Animation in = AnimationUtils.loadAnimation(this,android.R.anim.fade_in);
@@ -168,6 +187,13 @@ public final class MainActivity extends AppCompatActivity implements LoaderManag
             result = 0;
             operations.newOp();
             updateUI();
+            /*
+            new AlertDialog.Builder(this)
+                    .setMessage(operations.op1+" op "+operations.op2 + " = "+operations.result)
+                    .setPositiveButton(android.R.string.ok, null)
+                    .create()
+                    .show();
+                    */
         }
     }
     private int result = 0;
@@ -204,10 +230,23 @@ public final class MainActivity extends AppCompatActivity implements LoaderManag
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
+                findViewById(R.id.imgViewSmileOk).setVisibility(View.VISIBLE);
+            }
+        }, 500);
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                findViewById(R.id.imgViewSmileOk).setVisibility(View.GONE);
+            }
+        }, 2500);
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
                 newOperation();
             }
-        }, 1000);
+        }, 3000);
     }
+
     @UiThread
     private void lose()
     {
@@ -219,19 +258,31 @@ public final class MainActivity extends AppCompatActivity implements LoaderManag
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
+                findViewById(R.id.imgViewSmileNo).setVisibility(View.VISIBLE);
+            }
+        }, 500);
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                findViewById(R.id.imgViewSmileNo).setVisibility(View.GONE);
+            }
+        }, 2500);
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
                 newOperation();
             }
-        }, 1000);
+        }, 3000);
     }
 
     private void setDigit(@IdRes final int tvId, final int digit, final int number){
         final int n0 = number / (int)java.lang.Math.pow(10, digit);
         final int n1 = n0 % 10;
         if (n0==0 && n1==0 && digit>0) {
-            ((TextView)findViewById(tvId)).setText(" ");
+            ((TextSwitcher)findViewById(tvId)).setText(" ");
         } else {
             final String s = Integer.toString(n1);
-            ((TextView) findViewById(tvId)).setText(s);
+            ((TextSwitcher) findViewById(tvId)).setText(s);
         }
 
     }
@@ -244,19 +295,72 @@ public final class MainActivity extends AppCompatActivity implements LoaderManag
 
         final boolean isSub = operations.getOpType()==Operations.OP_SUB_1 &&
                 operations.getOpType()==Operations.OP_SUB_2;
+        final boolean isOneDigitOperation = operations.getOpType()==Operations.OP_SUB_1 &&
+                operations.getOpType()==Operations.OP_ADD_1;
+
         ((TextView)findViewById(R.id.textCurrentOperation)).setText(Operations.OPNAMES[operations.getOpType()]);
+        ((TextSwitcher)findViewById(R.id.textView1_sign)).setText(isSub?"-":"+");
+        ((TextSwitcher)findViewById(R.id.textView2_sign)).setText("=");
         setDigit(R.id.textView1_1, 0, operations.op1);
         setDigit(R.id.textView1_2, 1, operations.op1);
 
         setDigit(R.id.textView2_1, 0, operations.op2);
         setDigit(R.id.textView2_2, 1, operations.op2);
 
-        setDigit(R.id.textView3_1, 0, result);
-        setDigit(R.id.textView3_2, 1, result);
-        setDigit(R.id.textView3_3, 2, result);
+        if (result==0) {
+            ((TextSwitcher)findViewById(R.id.textView3_1)).setText("");
+            ((TextSwitcher)findViewById(R.id.textView3_2)).setText("");
+            ((TextSwitcher)findViewById(R.id.textView3_3)).setText("");
+        } else {
+            setDigit(R.id.textView3_1, 0, result);
+            setDigit(R.id.textView3_2, 1, result);
+            setDigit(R.id.textView3_3, 2, result);
+        }
 
         final Math math = Math.getInstance(this);
         ((TextSwitcher)findViewById(R.id.textSwitcerPoints)).setText(Integer.toString(math.getPoints()));
+
+        final int primColor = ContextCompat.getColor(this, R.color.colorPrimaryDark);
+        for (final @IdRes int tvid: textSwitchersOp) {
+            final TextSwitcher ts = findViewById(tvid);
+            ((TextView)ts.getCurrentView()).setTextColor(primColor);
+            ((TextView)ts.getNextView()).setTextColor(primColor);
+        }
+        for (final @IdRes int tvid: textSwitchersRes) {
+            final TextSwitcher ts = findViewById(tvid);
+            ((TextView)ts.getCurrentView()).setTextColor(primColor);
+            ((TextView)ts.getNextView()).setTextColor(primColor);
+        }
+
+        final int selColor = ContextCompat.getColor(this, R.color.colorAccent);
+        @IdRes int tv1, tv2, tv3;
+        TextSwitcher ts;
+        if (result==0){
+            tv1 = R.id.textView1_1;
+            tv2 = R.id.textView2_1;
+            tv3 = R.id.textView3_1;
+        } else {
+            tv1 = R.id.textView1_2;
+            tv2 = R.id.textView2_2;
+            tv3 = R.id.textView3_2;
+            final int numDigits = ((int) java.lang.Math.log10(result)) + 1;
+            if (numDigits>2 && result!=operations.result) {
+                ts = ((TextSwitcher)findViewById(R.id.textView3_3));
+                ts.setText("?");
+                ((TextView)ts.getCurrentView()).setTextColor(Color.LTGRAY);
+            }
+        }
+        if (!isOneDigitOperation) {
+            ts = ((TextSwitcher) findViewById(tv1));
+            ((TextView) ts.getCurrentView()).setTextColor(selColor);
+            ts = ((TextSwitcher) findViewById(tv2));
+            ((TextView) ts.getCurrentView()).setTextColor(selColor);
+        }
+        if (result!=operations.result) {
+            ts = ((TextSwitcher) findViewById(tv3));
+            ts.setText("?");
+            ((TextView) ts.getCurrentView()).setTextColor(Color.LTGRAY);
+        }
     }
 
 }
