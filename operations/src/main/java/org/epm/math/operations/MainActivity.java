d20eea944c3cbe1ba5c78dc8c125928b58088b37
Emilio Mancini <epmancini@gmail.com>
2017-08-06 18:06:23 +0200

    Immersive screen. Text size.

--- operations/src/main/java/org/epm/math/operations/MainActivity.java
+++ operations/src/main/java/org/epm/math/operations/MainActivity.java
@@ -91,15 +91,6 @@ public final class MainActivity extends AppCompatActivity implements LoaderManag
             findViewById(tvid).setOnClickListener(deleteClickListener);
             initTextSwitcher(tvid, textSwitcherFactoryOps);
         }
-        /*
-        findViewById(R.id.buttonChangeOperation).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                operations.changeOpType();
-                newOperation();
-            }
-        });
-        */
 
         initTextSwitcher(R.id.textSwitcherPoints, textSwitcherFactoryPoints);
 
@@ -560,6 +551,20 @@ public final class MainActivity extends AppCompatActivity implements LoaderManag
                 setDigit(tvLeft, 2, number);
         }
 
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
     }
 
 }
